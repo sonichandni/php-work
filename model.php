fddc220f8c93e9581b46bfcbe3b8953cd340854b
Chandni-Soni <chandni.soni@bacancytechnology.com>
2018-12-28 12:49:34 +0530

    Second Commit1'

---
+++ model.php
@@ -0,0 +1,38 @@
+<?php
+    include 'Connection.php';
+    $con1=new Connection();
+    $con=$con1->mkConnection();
+    
+    class model
+    {
+        function insert($con,$stu,$table)
+        {
+            $k=array_keys($stu);
+            $col=implode(",",$k);
+            
+            $v=array_values($stu);
+            $val=implode("','",$v);
+            
+            $q="insert into $table($col) values('$val')";
+           //echo $q;exit;
+            return $con->query($q);
+        }
+        function select($con,$table,$where)
+        {
+            $q="select * from $table where 1=1";
+            foreach($where as $k=>$v)
+            {
+                $q.=" and $k='$v'";
+            }
+           //echo $q;exit;
+            //alert($q);
+            $all1=$con->query($q);
+            
+            while($row=$all1->fetch_object())
+            {
+                $r[]=$row;
+            }
+            if(isset($r))
+                return $r;
+        }
+?>
