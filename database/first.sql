e2976989cf77b0812e7b365514d6624e77fe6bd4
Chandni-Soni <chandni.soni@bacancytechnology.com>
2019-01-09 19:03:15 +0530

    updated database added

--- database/first.sql
+++ database/first.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 08, 2019 at 10:40 AM
+-- Generation Time: Jan 09, 2019 at 02:31 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 5.6.37
 
@@ -21,6 +21,50 @@ SET time_zone = "+00:00";
 --
 -- Database: `first`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `cid` int(11) NOT NULL,
+  `comm` varchar(250) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`cid`, `comm`, `uid`, `pid`) VALUES
+(2, 'nice mobile...\r\ngreat price...', 2, 2),
+(3, 'nice one', 2, 1),
+(4, 'great', 2, 3),
+(5, 'great 1..', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `pid` int(11) NOT NULL,
+  `pname` varchar(30) NOT NULL,
+  `pimg` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`pid`, `pname`, `pimg`) VALUES
+(1, 'm1', 'm1.jpg'),
+(2, 'm2', 'm2.jpg'),
+(3, 'm3', 'm3.jpg');
 
 -- --------------------------------------------------------
 
@@ -45,12 +89,24 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`uid`, `first_name`, `last_name`, `email`, `profile_pic`, `pwd`, `date_created`) VALUES
 (1, 'wdsqw', 'jhjh', 'hho@gmaik.com', '', '123', '2018-12-31'),
 (2, 'Chandni V.', 'Soni', 'cs@gmail.com', 'sm.png', 'Chandni!11', '0000-00-00'),
-(3, 'Diya', 'Soni', 'soni.chandni.415@gmail.com', '', 'Chandni@2', '0000-00-00'),
+(3, 'Diya', 'Sharma', 'soni.chandni.415@gmail.com', '', 'Chandni@2', '0000-00-00'),
 (4, 'Maya', 'Khanna', 'maya@gmail.com', '', 'maya1', '2018-12-31');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`pid`);
 
 --
 -- Indexes for table `user`
@@ -63,10 +119,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
