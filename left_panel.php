3ec3a5198bf653b87eb8632f3e364f84015b4369
Chandni-Soni <chandni.soni@bacancytechnology.com>
2019-01-11 11:12:22 +0530

    Added profile pic in left panel

--- left_panel.php
+++ left_panel.php
@@ -1,3 +1,11 @@
+<?php
+foreach($_SESSION["logged"] as $v)
+{
+	echo '<img src = "./user_images/'.$v->profile_pic.'">';
+	echo $v->email;
+}
+?>
+<hr>
 <br><a href="user_list.php">Users</a><br><br>
 <a href="profile_update.php">My Profile</a><br><br>
 <a href="product.php">Products</a>
